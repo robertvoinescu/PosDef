b922bb8612f66ae1fc98d0bce570ef747c05dee0
Robert Voinescu <rvo67@ascendanalytics.com>
2021-10-01 13:05:26 -0600

    nrg stuff

--- nrg-configuration/PosDefRunIt.py
+++ nrg-configuration/PosDefRunIt.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import sys
-import logging
 
 def isit_corr(C,eigval):
     psd = True
@@ -20,6 +19,7 @@ def isit_corr(C,eigval):
     return psd
 
 def PSD_reb(C,eigen_lt,eigen_replace):
+   print(eigen_replace,eigen_lt)
    C_sym = .5*(C+C.T)
    eigval, eigvec = np.linalg.eigh(C_sym)
    eigval_cutoff = np.diag([eigen_replace if val<eigen_lt else val for val in eigval])
@@ -30,13 +30,13 @@ def PSD_reb(C,eigen_lt,eigen_replace):
 
    return(C_prime)
 
-def PSD_loop(corr,eigen_lt,eigen_replace,max_iter,final_reb):
+def PSD_loop(corr,eigen_lt,eigen_replace,max_iter):
     corr = .5*(corr+corr.T)
     for i in range(0,max_iter):
-        logging.debug(f'Iteration: {i}')
+        print('Iteration: ',i)
         eigval, eigvec = np.linalg.eigh(corr)
         if isit_corr(corr,eigval):
-            logging.debug(f'Looping converged in {i} iterations.')
+            print(f'Looping converged in {i} iterations.')
             return corr
   
         eigval_cutoff = np.diag([eigen_replace if val<eigen_lt else val for val in eigval])
@@ -44,38 +44,31 @@ def PSD_loop(corr,eigen_lt,eigen_replace,max_iter,final_reb):
         np.fill_diagonal(corr,1)
         corr = np.clip(corr,-1,1)
 
-    if final_reb == 1:
-        logging.debug(f'Looping didn\'t converge in {max_iter} iterations, applying Rebanato\'s method.')
-        corr = PSD_reb(corr,eigen_lt=eigen_lt,eigen_replace=eigen_replace)
-
+    print(f'Looping didn\'t converge in {max_iter} iterations, applying Rebanato\'s method.')
+    corr = PSD_reb(corr,eigen_lt=eigen_lt,eigen_replace=eigen_replace)
     return corr
 
-def PSD_approx(file_name,work_path,name_col,output_table,eigen_lt,eigen_replace,max_iter,method,final_reb):
+def PSD_approx(file_name,work_path,name_col,output_table,eigen_lt,eigen_replace,max_iter,method):
     # convert to numpy array
     df_input_matrix = pd.read_csv(work_path+'\\'+file_name+'.csv')
-
-    # ensure our name_col has the right case
-    col_list =  df_input_matrix.columns.to_list()
-    name_col = col_list[[str(col).lower() for col in col_list].index(str.lower(name_col))]
-
     names = df_input_matrix.loc[pd.isna(df_input_matrix[name_col]) == False][name_col]
     input_matrix = df_input_matrix.loc[pd.isna(df_input_matrix[name_col]) == False, names].copy()
     input_matrix = input_matrix.fillna(value=0.0).to_numpy()
 
     # convert input_matrix to a correlation matrix if it isn't
     if  np.linalg.norm(np.diag(input_matrix)-1)>1e-6:
-        logging.debug('You\'ve inputed a covariance matrix.')
+        print('You\'ve inputed a covariance matrix.')
         n = input_matrix.shape[0]
         var_list = np.array([np.sqrt(input_matrix[i,i]) for i in range(n)])
         corr = np.array([[input_matrix[i, j]/(var_list[i]*var_list[j]) for i in range(n)] for j in range(n)])
         is_input_corr = False
     else:
-        logging.debug('You\'ve inputed a correlation matrix.')
+        print('You\'ve inputed a correlation matrix.')
         corr = input_matrix
         is_input_corr = True
     
     if method == 1:
-        corr_out = PSD_loop(corr,eigen_lt=eigen_lt,eigen_replace=eigen_replace,max_iter=max_iter,final_reb=final_reb)
+        corr_out = PSD_loop(corr,eigen_lt=eigen_lt,eigen_replace=eigen_replace,max_iter=max_iter)
     else:
         corr_out = PSD_reb(corr,eigen_lt=eigen_lt,eigen_replace=eigen_replace)
 
@@ -90,42 +83,27 @@ def PSD_approx(file_name,work_path,name_col,output_table,eigen_lt,eigen_replace,
 
     return df_input_matrix
 
-if __name__ == "__main__":
-    input_file_name = sys.argv[1]
-    param_name       = sys.argv[2]
-    work_path       = sys.argv[3]
-    try:
-        logging.basicConfig(level=logging.DEBUG,filename=work_path+'\\'+'python.log',  format='%(levelname)s - %(message)s')
-    except:
-        logging.basicConfig(level=logging.DEBUG,filename='python.log', filemode='w', format='%(levelname)s - %(message)s')
-    logging.debug('\n\nRUNNING POSDEF\n\n')
 
-    try:
-        params = pd.read_csv(work_path+'\\'+param_name+'.csv')
-        
-        # make naming conventions uniform
-        params.columns = map(str.lower, params.columns)
-        params = params.rename(columns=dict(
-            outputtable  = 'output_table',
-            namecol      = 'name_col',
-            eigenlt      = 'eigen_lt',
-            eigenreplace = 'eigen_replace',
-            maxiter      = 'max_iter',
-            method       = 'method',
-            finalreb    = 'final_reb'
-            ))
-        
-        params.max_iter=int(params.max_iter)
-        params.method=int(params.method)
-        params.final_reb=int(params.final_reb)
+input_file_name = sys.argv[1]
+parm_name       = sys.argv[2]
+work_path       = sys.argv[3]
+parms = pd.read_csv(work_path+'\\'+parm_name+'.csv')
 
-        logging.debug('System Arguments: \n'+str(sys.argv))
-        logging.debug('Params: \n'+str(params))
+# make naming conventions uniform
+parms.columns = map(str.lower, parms.columns)
+parms = parms.rename(columns=dict(
+    outputtable  = 'output_table',
+    namecol      = 'name_col',
+    eigenlt      = 'eigen_lt',
+    eigenreplace = 'eigen_replace',
+    maxiter      = 'max_iter',
+    method       = 'method'
+    ))
 
-        PSD_approx(input_file_name,work_path,**params.to_dict('r')[0])
-    except Exception as e:
-        logging.error(f'\n{e}')
-        logging.error(f'TRACE:\n{e.__traceback__.tb_frame}')
-    
+parms.max_iter=int(parms.max_iter)
+parms.method=int(parms.method)
+PSD_approx(input_file_name,work_path,**parms.to_dict('r')[0])
+
 # Example Run:
-# python .\PosDefRunIt.py NewCorrMatrix PosDefParams .\tabular_data\
+# python .\PosDefRunIt.py big3000 PosDefParms C:\Users\rvo67\Desktop\PosDefRunIt-to_python-updt
+
--- nrg-configuration/PosDefRunIt.sas
+++ nrg-configuration/PosDefRunIt.sas
@@ -3,7 +3,7 @@
 			NameCol=_name_,
 			eigenLT=0.001,
 			eigenReplace=0.01,
-			maxIter=1000,
+			maxIter=12,
 			method = 1);
 			
 	%if %symexist(PythonPosDefCounter)=0 %then %do;
@@ -16,18 +16,17 @@
 	%let PosDefSubDir = Powersimm\sasmacro\ForwardPriceSim;
 	%let WorkDirectory = %sysfunc(getoption(work));
 
-    data PosDefParams;
+    data PosDefParms;
 		OutputTable     = "&OutputTable.&PythonPosDefCounter."    ;
 		NameCol         = "&NameCol."        ;
-		eigenLT         =  &eigenLT.         ;
-		eigenReplace    =  &eigenReplace.    ;
-		maxIter         =  &maxIter.         ;
+		eigenLT         =  0.0000000000000001;
+		eigenReplace    =  0.000000000000001 ;
+		maxIter         =  1000.             ;
 		method          =  &method.          ;
-		final_reb	=  0		     ;
     run;
 
     
-   	proc export data=PosDefParams outfile= "&WorkDirectory.\PosDefParams&PythonPosDefCounter..csv" dbms=csv replace;
+   	proc export data=PosDefParms outfile= "&WorkDirectory.\PosDefParms&PythonPosDefCounter..csv" dbms=csv replace;
 	run;
 
 	proc export data=&InputTable. outfile= "&WorkDirectory.\&InputTable.&PythonPosDefCounter..csv" dbms=csv replace;
@@ -38,7 +37,7 @@
 	data _null_;
 		file pos;
 		pythonpath = %sysfunc(quote("C:\Program Files\Python37\python.exe"));
-		msgline = pythonpath || " &BookMacroCodeBase.\&PosDefSubDir.\PosDefRunIt.py &InputTable.&PythonPosDefCounter. PosDefParams&PythonPosDefCounter. &WorkDirectory.";
+		msgline = pythonpath || " &ASCENDROOT.\&PosDefSubDir.\PosDefRunIt.py &InputTable.&PythonPosDefCounter. PosDefParms&PythonPosDefCounter. &WorkDirectory.";
 		put msgline; 
 	run;
 
@@ -46,7 +45,12 @@
 	/* RV: call python executable to do find nearest covariance matrix requires numpy and pandas installed*/
 	options noxwait xsync;
 	x "&WorkDirectory.\RunPythonPosDef&PythonPosDefCounter..bat";
-
+	
+	/*
+	data _null_;
+		x=sleep(10);
+	run;
+	*/
 	
 	/* RV: Read out the python data */
 	proc import datafile="&WorkDirectory.\&OutputTable.&PythonPosDefCounter..csv" out=&OutputTable. dbms=csv replace;
